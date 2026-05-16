d95bb740ce0473bcbf5b87b6e7318d2712cc97f4
TESTER <120787636+Riders004@users.noreply.github.com>
2026-05-16 11:10:23 +0530

    Delete wallpaper.bat

--- wallpaper.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Windows RDP Environment Initialized.
-:: Removed suspicious registry modifications and external downloads.
-:: This script can be used for harmless environment customization if needed.
